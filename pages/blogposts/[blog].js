8b73ca69538598b9a5c1850ba87ab4cb84a45e66
Vikas Kumar <vk.pratap98@gmail.com>
2022-03-18 17:51:14 +0530

    statically generated all the blog post pages

--- pages/blogposts/[blog].js
+++ pages/blogposts/[blog].js
@@ -1,14 +1,35 @@
 import { useRouter } from 'next/router';
 import React from 'react'
 
-const Blog = () => {
-  const router = useRouter();
-  const {blog} = router.query;
+const Blog = (props) => {
+  const post = props.post;
+
   return (
-    <div>
-      <h1>{blog}</h1>
+    <div className='p-10'>
+      <h1 className='text-center font-bold'>{post.title}</h1>
+      <p className='py-3'>{post.content}</p>
     </div>
   )
 }
 
 export default Blog;
+
+export async function getStaticPaths() {
+  const res = await fetch(`http://localhost:3000/api/getblogs`);
+  const posts = await res.json()
+  const paths = posts.map((post) => ({
+    params: { blog: post.slug },
+  }))
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false }
+}
+
+// This also gets called at build time
+export async function getStaticProps({ params }) {
+  const res = await fetch(`http://localhost:3000/api/blogpost?blog=${params.blog}`)
+  const post = await res.json()
+
+  // Pass post data to the page via props
+  return { props: { post } }
+}
