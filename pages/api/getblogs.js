bca8e67bd705c26a2b6f4a3e8720dbf1dd3a158c
Vikas Kumar <vk.pratap98@gmail.com>
2022-03-18 16:41:42 +0530

    all blogs data is fetched and using ssr rendered the data

--- pages/api/getblogs.js
+++ pages/api/getblogs.js
@@ -1,14 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as fs from 'fs';
 
-export default function handler(req, res) {
-  fs.readdir("blogsData", (err, data)=>{
-    if(err){
-      res.status(404).json({message: err.message});
+export default async function handler(req, res) {
+  try{
+    let allFilesData = [];
+    let allFiles = await fs.promises.readdir("blogsData");
+    for (const file of allFiles) {
+      let data = await fs.promises.readFile(`blogsData/${file}`, 'utf-8');
+      allFilesData.push(JSON.parse(data));
     }
-    else{
-      console.log(data);
-      res.status(200).json(data);
-    }
-  })
+    res.status(200).json(allFilesData);
+  }
+  catch(err){
+    res.status(404).json({message: err.message});
+  }
+
 }
--- pages/blogposts/index.js
+++ pages/blogposts/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
-const BlogPosts = () => {
+const BlogPosts = (props) => {
+  let allBlogs = props.data;
+
   return (
-    <div>
-      Learning begins here. Search for any type of article here.
-      <Link href={`/blogposts/${"howtolearnjs"}`} >
-        <a>
-          Learn js easy way...
-        </a>
-      </Link>
+    <div className="max-w-[920px] mx-auto px-10 py-10">
+      <h2 className="text-xl font-semibold">
+        Learning begins here. Find latest articles below.
+      </h2>
+      <div className="flex flex-col gap-4 mt-4">
+        {allBlogs.map((blog, idx) => {
+          return (
+            <div key={idx}>
+              <Link href={`/blogposts/${blog.slug}`}>
+                <a>
+                  <h3 className="font-semibold">{blog.title}</h3>
+                  <p>{blog.content.slice(0,140)}...</p>
+                </a>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default BlogPosts;
+
+export async function getStaticProps() {
+  const res = await fetch(`http://localhost:3000/api/getblogs`);
+  const data = await res.json();
+  return {
+    props: {
+      data,
+    },
+  };
+}
