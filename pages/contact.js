d2bbb7dcf3d04f533efbb6d5876c2f13c48f4545
Vikas Kumar <vk.pratap98@gmail.com>
2022-03-22 23:19:57 +0530

    basic form structure created for contact page

--- pages/contact.js
+++ pages/contact.js
@@ -2,7 +2,28 @@ import React from 'react'
 
 const ContactPage = () => {
   return (
-    <div>Contact Us page</div>
+    <div>
+      <h1>Contact Us</h1>
+      <p>Fill your details</p>
+      <form>
+        <div>
+          <label htmlFor="nameInp">Name</label>
+          <input id='nameInp' type="text" placeholder='Enter your name'/>
+        </div>
+        <div>
+          <label htmlFor="emailInp">Email</label>
+          <input id='emailInp' type="email" placeholder="Enter your email" />
+        </div>
+        <div>
+          <label htmlFor="mobileNoInp">Mobile No</label>
+          <input id='mobileNoInp' placeholder='Enter your phone no'/>
+        </div>
+        <div>
+          <label htmlFor="descInp"></label>
+          <textarea id="descInp" placeholder='Enter your query.' />
+        </div>
+      </form> 
+    </div>
   )
 }
 
